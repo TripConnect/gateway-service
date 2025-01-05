b158552989d56848e2c2eda02823eb2e7870c733
ndtrong <nguyenductrong12a1@gmail.com>
2025-01-05 13:13:12 +0700

    Update: Handle for ends live

--- src/application.ts
+++ src/application.ts
@@ -13,12 +13,12 @@ import morgan from 'morgan';
 import { graphqlUploadExpress } from 'graphql-upload-ts';
 const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
 const ffmpeg = require('fluent-ffmpeg');
+import { PassThrough } from 'stream';
 import { instrument } from "@socket.io/admin-ui";
 
 import gqlServer from './services/graphql';
 import ChatService from './services/grpc/chatService';
 import logger from './utils/logging';
-import { PassThrough } from 'stream';
 
 ffmpeg.setFfmpegPath(ffmpegPath);
 
@@ -32,10 +32,20 @@ const io = new Server(server, {
 
 const PORT = process.env.GATEWAY_SERVICE_PORT || 31071;
 const PUBLIC_PATH = path.join(__dirname, 'public');
-const chatNamespace = io.of('/chat');
-const livestreamNamespace = io.of('/livestream');
 const accessLogStream = fs.createWriteStream(path.join(__dirname, '../log/access.log'), { flags: 'a' });
 
+const chatNamespace = io.of('/chat');
+
+const livesNamespace = io.of('/livestream');
+const LIVESTREAM_FOLDER = 'livestreams';
+const HLS_PATH = path.join(PUBLIC_PATH, LIVESTREAM_FOLDER);
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms', { stream: accessLogStream }));
 app.use(express.static(PUBLIC_PATH));
 app.use(graphqlUploadExpress({ maxFileSize: 10000000, maxFiles: 10 }))
@@ -83,10 +93,7 @@ chatNamespace.on("connection", async (socket) => {
     });
 });
 
-livestreamNamespace.on("connection", async (socket) => {
-    const livestreamFolder = 'livestreams';
-    const HLS_PATH = path.join(PUBLIC_PATH, livestreamFolder);
-
+livesNamespace.on("connection", async (socket) => {
     let { token } = socket.handshake.auth;
     if (!token) {
         socket.disconnect(true);
@@ -94,47 +101,69 @@ livestreamNamespace.on("connection", async (socket) => {
         return;
     }
     let { userId } = jwt.verify(token, process.env.JWT_SECRET_KEY || "") as { userId: string };
+    console.info({ message: "Livestream socket connected", userId });
+
     socket.data.userId = userId;
-    socket.data.inputStream = new PassThrough();
+    const inputStream = new PassThrough();
+
+    socket.on("start", async (event, callback: Function) => {
+        try {
+            let { roomId } = event;
+            logger.info({ message: "Start lives", roomId });
+
+            let livestreamDir = path.join(HLS_PATH, roomId);
+
+            if (!fs.existsSync(livestreamDir)) {
+                fs.mkdirSync(livestreamDir, { recursive: true });
+            }
+
+            // Start FFmpeg to convert the WebSocket stream to HLS
+            let ffmpegCommand = ffmpeg(inputStream)
+                .inputFormat('webm')
+                .videoCodec('copy')
+                .audioCodec('copy')
+                .outputOptions([
+                    '-hls_time 10',
+                    '-hls_list_size 0',
+                    '-hls_flags delete_segments+append_list',
+                ])
+                .output(path.join(livestreamDir, 'streaming.m3u8'))
+                .on('start', () => {
+                    console.log(`FFmpeg started for livestream ID: ${roomId}`);
+                })
+                .on('error', (err: any) => {
+                    console.error(`FFmpeg error for livestream ID ${roomId}:`, err);
+                })
+                .run(); // Start FFmpeg
+            // callback({ status: 'DONE' });
+        } catch (error: any) {
+            logger.error({ message: 'Cannot saving hls segment', error });
+            logger.error(error.message);
+            // callback({ status: 'ERROR' });
+        }
+    });
 
     socket.on("segment", async (event, callback: Function) => {
         try {
             let { roomId, segment } = event;
-            logger.info({ message: "Record segment for " + roomId, "segment": segment });
-            socket.data.inputStream.write(segment);
+            logger.info({ message: "Record segment", roomId });
+            inputStream.write(segment);
+            // callback({ status: 'DONE' });
         } catch (error) {
             logger.error('Error while saving livestream segment');
+            // callback({ status: 'ERROR' });
         }
     });
 
-    socket.on("start", async (event, callback: Function) => {
-        let { roomId } = event;
-        logger.info({ message: "Start lives", roomId });
-
-        let livestreamDir = path.join(HLS_PATH, roomId);
-
-        if (!fs.existsSync(livestreamDir)) {
-            fs.mkdirSync(livestreamDir, { recursive: true });
+    socket.on("disconnect", async (event, callback: Function) => {
+        try {
+            logger.info({ message: "Record segment ends" });
+            inputStream.end();
+            // callback({ status: 'DONE' });
+        } catch (error) {
+            logger.error('Ends livestream');
+            // callback({ status: 'ERROR' });
         }
-
-        // Start FFmpeg to convert the WebSocket stream to HLS
-        let ffmpegCommand = ffmpeg(socket.data.inputStream)
-            .inputFormat('webm')
-            .videoCodec('copy')
-            .audioCodec('copy')
-            .outputOptions([
-                '-hls_time 10',
-                '-hls_list_size 0',
-                '-hls_flags delete_segments+append_list',
-            ])
-            .output(path.join(livestreamDir, 'streaming.m3u8'))
-            .on('start', () => {
-                console.log(`FFmpeg started for livestream ID: ${roomId}`);
-            })
-            .on('error', (err: any) => {
-                console.error(`FFmpeg error for livestream ID ${roomId}:`, err);
-            })
-            .run(); // Start FFmpeg
     });
 });
 
